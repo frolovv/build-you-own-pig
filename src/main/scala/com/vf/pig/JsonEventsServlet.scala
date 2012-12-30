ec8b08d25b4c5f1350bb2abc6c7caf6abc8fab1d
valerym <valeryf@wix.com>
2012-12-31 01:29:10 +0200

    - refactored pom - removed unneeded code that was taken out to wql2pig project - cleaned up html code - cleaned up wix pig emitter and json servlet

--- src/main/scala/com/vf/pig/JsonEventsServlet.scala
+++ src/main/scala/com/vf/pig/JsonEventsServlet.scala
@@ -2,6 +2,7 @@ package com.vf.pig
 
 import javax.servlet.http.{HttpServlet, HttpServletResponse, HttpServletRequest}
 import util.parsing.json.JSONObject
+
 /**
  * User: valeryf
  * Date: 12/12/12 7:05 PM
@@ -13,13 +14,9 @@ class JsonEventsServlet extends HttpServlet {
   }
 
   private def generateResponse(req: HttpServletRequest): Option[String] = {
-    val src = req.getParameter("src").toInt
-    val evids = (req.getParameter("evids").split(",").toList) map {case x : String => x.trim().toInt}
-    val startDate = req.getParameter("startDate")
-    val endDate = req.getParameter("endDate")
-    val fields = (req.getParameter("fields").split(",").toList) map {case x : String => x.trim()}
+    val wql = req.getParameter("wql")
 
-    val emitter = new WixPigEmitter(src, evids, startDate, endDate, fields)
+    val emitter = new WixPigEmitter(wql)
 
     val response = JSONObject(
       Map("pig" -> emitter.emit)
--- src/main/scala/com/vf/pig/WixPigEmitter.scala
+++ src/main/scala/com/vf/pig/WixPigEmitter.scala
@@ -1,45 +1,17 @@
 package com.vf.pig
 
+import com.vf.wql.parser.WqlParser
+import parser.{Wql2Pig, PigPrinter}
+
 /**
  * User: valeryf
  * Date: 12/12/12 11:25 PM
  */
-class WixPigEmitter(src: Int, evids: List[Int], start: String, end: String, fields: List[String]) {
-
-  def emitColumnFilter(evids: List[Int]): ConditionExpr = {
-    evids match {
-      case Nil => EmptyCondition()
-      case _ => {
-        val mapped = evids map {
-          evid => SimpleCondition(ColumnFilterEquality(), FieldExpr("event:evid"), NumberExpr(evid))
-        }
-        OrExpr(mapped)
-      }
-    }
-  }
-
-  def emitTblLoader(src: Int, evids: List[Int], start: String, stop: String, fields: List[String]): AbstractUdf = {
-    val columnFilter = emitColumnFilter(evids)
-    WixTableLoaderExpr("users_by_src", KeyFilterExpr(start, stop, src), ColumnFilterExpr(columnFilter), fields)
-  }
-
-  def emitSchemaOf(names: List[String]): SchemaExpr = {
-    val types = names map {
-      case x if List("evid", "date_created", "src").contains(x) => "long"
-      case _ => "chararray"
-    }
-    SchemaExpr(names, types)
-  }
+class WixPigEmitter(wql: String) extends PigPrinter with WqlParser with Wql2Pig {
 
   def emit(): String = {
-
-    val tblLoader = emitTblLoader(src, evids, start, end, fields)
-    val schema = emitSchemaOf(fields)
-
-    val expr1 = AssignExpr(VarExpr("events"), LoadExpr(SymbolicExpr("wix-bi"), tblLoader, schema))
-    val expr2 = DumpExpr(VarExpr("events"))
-    val exprs = List(expr1, expr2)
-
-    Printer.exprsToString(exprs)
+    val wqls = this.parse(wql)
+    val pigs = this.pigify(wqls)
+    this.exprsToString(pigs)
   }
 }
